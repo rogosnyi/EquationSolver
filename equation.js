3c9a4d14b84caeedfe956eebcd398d6d5765af34
rogosnyi <lesha3320@gmail.com>
2022-02-20 16:52:58 +0200

    implemented interactive mode + minor fixes

--- equation.js
+++ equation.js
@@ -12,8 +12,11 @@ if (start) {
   console.log(`Equation is: ${values[0]}x^2 + ${values[1]}x + ${values[2]} `);
   equationSolve(values);
 } else {
-  // do interactive mode
-  console.log("non interactive");
+  console.log("Selected interactive mode\n");
+
+  const values = parceConsole();
+  console.log(`\nEquation is: ${values[0]}x^2 + ${values[1]}x + ${values[2]} `);
+  equationSolve(values);
 }
 
 function parceFile(content) {
@@ -21,6 +24,43 @@ function parceFile(content) {
   for (let i = 0; i < result.length; i++) {
     result[i] = Number(result[i]);
   }
+  return result;
+}
+
+function parceConsole() {
+  const result = [];
+  let a, b, c;
+  let userInput;
+  do {
+    userInput = readline.question("Enter a: ");
+    a = Number(userInput);
+    if (isNaN(a)) {
+      console.log("Input error. Value must me a number");
+    } else {
+      result.push(a);
+    }
+  } while (isNaN(a));
+
+  do {
+    userInput = readline.question("Enter b: ");
+    b = Number(userInput);
+    if (isNaN(b)) {
+      console.log("Input error. Value must me a number");
+    } else {
+      result.push(b);
+    }
+  } while (isNaN(b));
+
+  do {
+    userInput = readline.question("Enter c: ");
+    c = Number(userInput);
+    if (isNaN(c)) {
+      console.log("Input error. Value must me a number");
+    } else {
+      result.push(c);
+    }
+  } while (isNaN(c));
+
   return result;
 }
 
@@ -35,7 +75,7 @@ function equationSolve(coef) {
     let x2 = (-1 * b - Math.sqrt(discriminant)) / (2 * a);
     if (x1 === x2) {
       console.log(`There's one equation root: ${x1}`);
-    } else console.log(`There's equation roots:\nx1 = ${x1}\nx2 = ${x2}`);
+    } else console.log(`There's two equation roots:\nx1 = ${x1}\nx2 = ${x2}`);
   } else {
     let x1 = (-1 * b + Math.sqrt(discriminant)) / (2 * a);
     console.log(`There's one equation root: ${x1}`);
